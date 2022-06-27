ab057e55c7bc616003931b1ffb9e4a087e39c705
clauggg <claudia.garcia.gil@estudiantat.upc.edu>
2022-06-28 00:46:48 +0200

    Canvi inventari

--- src/main/java/edu/upc/dsa/services/UserService.java
+++ src/main/java/edu/upc/dsa/services/UserService.java
@@ -301,7 +301,7 @@ public class UserService {
             @ApiResponse(code = 201, message = "Successful", response= Inventory.class, responseContainer="List"),
 
     })
-    @Path("inventory/{username}")
+    @Path("{username}/inventory")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getInventory(@PathParam("username")String username) {
 
